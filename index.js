9212491848bc5d2e13bed0052f97a36fc4fd3a32
harrydrewx <harrydonovan04@gmail.com>
2025-05-27 09:17:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -35,12 +35,7 @@ app.post("/apply", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
   console.log('Server running on port ${PORT}');
-});
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port ${PORT}');
 
